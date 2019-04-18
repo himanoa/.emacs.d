034d8567005962ad04fe9c9401865cd14da3494f
himanoa <matsunoappy@gmail.com>
2019-04-18 15:18:02 +0900

    Add org-mode setting

--- init.el
+++ init.el
@@ -10,3 +10,25 @@
 (set-default 'line-spacing 5)
 (show-paren-mode nil)
 (setq x-select-enable-clipboard t)
+(setq org-directory "~/Documents/")
+(setq org-default-notes-file "notes.org")
+
+
+(define-key global-map "\C-cc" 'org-capture)
+; Org-captureのテンプレート（メニュー）の設定
+(setq org-capture-templates
+      '(("n" "Note" entry (file+headline "~/Documents/notes.org" "Notes")
+         "* %?\nEntered on %U\n %i\n %a")
+        ))
+
+(defun show-org-buffer (file)
+  "Show an org-file FILE on the current buffer. "
+  (interactive)
+  (if (get-buffer file)
+      (let ((buffer (get-buffer file)))
+	(switch-to buffer buffer)
+	(message "%s" file))
+    (find-file (concat "~/Documents/notes.org" file))))
+(global-set-key (kbd "C-M-^") '(lambda () (interactive)
+				 (show-org-buffer
+				  "notes.org")))
