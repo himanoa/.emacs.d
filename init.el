cc239467a37903defc354cb377954520562e0490
himanoa <matsunoappy@gmail.com>
2019-04-18 15:05:14 +0900

    Disable spashscreen

--- init.el
+++ init.el
@@ -4,3 +4,8 @@
              '(font . "M+ 1mn-12"))
 (setq inhibit-splash-screen t)
 (global-set-key "\C-h" 'delete-backward-char)
+(prefer-coding-system 'utf-8)
+(set-default 'buffer-file-coding-system 'utf-8)
+(menu-bar-mode -1)
+(set-default 'line-spacing 5)
+(show-paren-mode nil)
